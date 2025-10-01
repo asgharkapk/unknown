2226de6666c02ab5931846ab365c1b9e342f4eae
asgharkapk <162083661+asgharkapk@users.noreply.github.com>
2025-10-01 08:37:20 +0330

    Update and rename a.ini to a.ini.py.cpp.md.m

--- a.ini.py.cpp.md.m
+++ a.ini.py.cpp.md.m
@@ -1,21 +1,29 @@
+videogame
+clash_rulebase_
+
 `[]🇨🇭Switzerland`[]🇳🇴Norway`[]🇫🇮Finland`[]🇧🇪Belgium`[]🇵🇹Portugal`[]🇮🇹Italy`[]🇨🇱Chile`[]🇦🇷Argentina`[]🇳🇿NewZealand`[]🇮🇩Indonesia
+`[]🇮🇸Iceland`[]🇩🇰Denmark`[]🇺🇦Ukraine`[]🏳️‍🌈RELAY
+  
+🇨🇭|CH|🇳🇴|NO|🇫🇮|FI|🇧🇪|BE|🇵🇹|PT|🇮🇹|IT|🇨🇱|CL|🇦🇷|AR|🇳🇿|NZ|🇮🇩|ID
+IS|🇮🇸|DK|🇩🇰|UA|🇺🇦|RELAY
+  
+      - 🇨🇭Switzerland
+      - 🇳🇴Norway
+      - 🇫🇮Finland
+      - 🇧🇪Belgium
+      - 🇵🇹Portugal
+      - 🇮🇹Italy
+      - 🇨🇱Chile
+      - 🇦🇷Argentina
+      - 🇳🇿NewZealand
+      - 🇮🇩Indonesia
 
-|🇨🇭|🇳🇴|🇫🇮|🇧🇪|🇵🇹|🇮🇹|🇨🇱|🇦🇷|🇳🇿|🇮🇩|
-|🇨🇭|CH|🇳🇴|NO|🇫🇮|FI|🇧🇪|BE|🇵🇹|PT|🇮🇹|IT|🇨🇱|CL|🇦🇷|AR|🇳🇿|NZ|🇮🇩|ID
+      - 🇮🇸Iceland
+      - 🇩🇰Denmark
+      - 🇺🇦Ukraine
+      - 🏳️‍🌈RELAY
 
-  - 🇨🇭Switzerland
-  - 🇳🇴Norway
-  - 🇫🇮Finland
-  - 🇧🇪Belgium
-  - 🇵🇹Portugal
-  - 🇮🇹Italy
-  - 🇨🇱Chile
-  - 🇦🇷Argentina
-  - 🇳🇿NewZealand
-  - 🇮🇩Indonesia
-
-US|🇺🇸|GB|🇬🇧|JP|🇯🇵|HK|🇭🇰|TW|🇹🇼|SG|🇸🇬|KR|🇰🇷|DE|🇩🇪|FR|🇫🇷|NL|🇳🇱|IN|🇮🇳|CA|🇨🇦|RU|🇷🇺|AU|🇦🇺|MY|🇲🇾|TH|🇹🇭|PH|🇵🇭|VN|🇻🇳|TR|🇹🇷|BR|🇧🇷|SA|🇸🇦|ZA|🇿🇦|AE|🇦🇪|MX|🇲🇽|LT|🇱🇹|MD|🇲🇩|PL|🇵🇱|SE|🇸🇪|IR|🇮🇷|ES|🇪🇸|CN|🇨🇳|🇨🇭|CH|🇳🇴|NO|🇫🇮|FI|🇧🇪|BE|🇵🇹|PT|🇮🇹|IT|🇨🇱|CL|🇦🇷|AR|🇳🇿|NZ|🇮🇩|ID
-
+  
 custom_proxy_group=🇨🇭Switzerland`url-test`(🇨🇭|CH|Switzerland|瑞士|苏黎世|Zurich|Geneva)`https://www.facebook.com`-1,,300
 custom_proxy_group=🇳🇴Norway`url-test`(🇳🇴|NO|Norway|挪威|奥斯陆|Oslo)`https://www.facebook.com`-1,,300
 custom_proxy_group=🇫🇮Finland`url-test`(🇫🇮|FI|Finland|芬兰|赫尔辛基|Helsinki)`https://www.facebook.com`-1,,300
@@ -26,6 +34,11 @@ custom_proxy_group=🇨🇱Chile`url-test`(🇨🇱|CL|Chile|智利|圣地亚哥
 custom_proxy_group=🇦🇷Argentina`url-test`(🇦🇷|AR|Argentina|阿根廷|布宜诺斯艾利斯|BuenosAires)`https://www.facebook.com`-1,,300
 custom_proxy_group=🇳🇿NewZealand`url-test`(🇳🇿|NZ|NewZealand|新西兰|奥克兰|Auckland|Wellington)`https://www.facebook.com`-1,,300
 custom_proxy_group=🇮🇩Indonesia`url-test`(🇮🇩|ID|Indonesia|印尼|雅加达|Jakarta|Bali)`https://www.facebook.com`-1,,300
+
+custom_proxy_group=🇮🇸Iceland`url-test`(🇮🇸|IS|Iceland)`https://www.facebook.com`-1,,300
+custom_proxy_group=🇩🇰Denmark`url-test`(🇩🇰|DK|Denmark)`https://www.facebook.com`-1,,300
+custom_proxy_group=🇺🇦Ukraine`url-test`(🇺🇦|UA|Ukraine)`https://www.facebook.com`-1,,300
+custom_proxy_group=🏳️‍🌈RELAY`url-test`(RELAY)`https://www.facebook.com`-1,,300
 
 
   - name: 🇨🇭Switzerland
@@ -95,6 +108,41 @@ custom_proxy_group=🇮🇩Indonesia`url-test`(🇮🇩|ID|Indonesia|印尼|雅�
     type: url-test
     url: https://www.facebook.com
     tolerance: 300
+    use:
+        - proxylist1
+    filter: "🇮🇩|ID|Indonesia|印尼|雅加达|Jakarta|Bali"
+
+
+        
+  - name: 🇮🇸Iceland
+    type: url-test
+    url: https://www.facebook.com
+    tolerance: 300
+    use:
+      - proxylist1
+    filter: "🇮🇸|IS|Iceland"
+  - name: 🇩🇰Denmark
+    type: url-test
+    url: https://www.facebook.com
+    tolerance: 300
+    use:
+      - proxylist1
+    filter: "🇩🇰|DK|Denmark"
+  - name: 🇺🇦Ukraine
+    type: url-test
+    url: https://www.facebook.com
+    tolerance: 300
+    use:
+      - proxylist1
+    filter: "🇺🇦|UA|Ukraine"
+  - name: 🏳️‍🌈RELAY
+    type: url-test
+    url: https://www.facebook.com
+    tolerance: 300
+    use:
+      - proxylist1
+    filter: "RELAY"
+
     use:
       - proxylist1
     filter: "🇮🇩|ID|Indonesia|印尼|雅加达|Jakarta|Bali"
